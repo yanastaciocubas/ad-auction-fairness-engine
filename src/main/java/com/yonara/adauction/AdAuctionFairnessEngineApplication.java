e86490abefd2b8a2c01d4af1fc7ffdb483654430
thehispanicsinsajo <yonaraanastacio@gmail.com>
2026-03-10 00:34:34 -0400

    add spring boot project structure

---
+++ src/main/java/com/yonara/adauction/AdAuctionFairnessEngineApplication.java
@@ -0,0 +1,13 @@
+package com.yonara.adauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdAuctionFairnessEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdAuctionFairnessEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yonara/adauction/AdAuctionFairnessEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonara.adauction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdAuctionFairnessEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
